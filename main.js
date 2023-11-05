52ebe4efcf95c20a5703495c91b45e82363b9f8d
Dagim Tasew <thisisdagim@gmail.com>
2023-11-04 22:30:52 -1200

    Deploying Existing Portfolio

---
+++ main.js
@@ -0,0 +1,52 @@
+const typed = new Typed('.text',{
+    strings:['Frontend Developer.','Programmer','###'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+const typedname = new Typed('.name',{
+    strings:['Dagim Tasew'],
+    typeSpeed:200,
+    backSpeed:100,
+    backDelay:10000,
+    loop:true
+});
+
+const typedage = new Typed('.age',{
+    strings:['19'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:10000,
+    loop:true
+});
+
+const typedjob = new Typed('.job',{
+    strings:['Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:10000,
+    loop:true
+});
+
+const typedlangueage = new Typed('.lang',{
+    strings:['HTML'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:10000,
+    loop:true
+});
+
+const typedservice = new Typed('.service',{
+    strings:['C E R T I F I E D'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:1
+});
+
+
+function say(){
+    alert("-Sorry , you can't access this web now \n -We are working on it")
+}
